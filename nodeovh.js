864325f530690f0ff758c5c05bbc6b29f5087be4
Elyes Ben Amor <amor@vorwaerts.com>
2020-08-04 15:32:00 +0200

    adding avalability

--- nodeovh.js
+++ nodeovh.js
@@ -1,4 +1,3 @@
-const request = require('request');
 
 require('dotenv').config();
 
@@ -8,9 +7,39 @@ const ovh = require('ovh')({
   appSecret: process.env.OVH_API_AS,
   consumerKey: process.env.OVH_API_CK,
 });
+async function Server(){
+    const available = await ovh.request('GET','/dedicated/server/availabilities',{
+        country : EU,
+        hardware : ''
+    }, function(err, valid){
+        if (valid){
+            return resolve(JSON.parse(body.valid));
+        }
+        else {
+            return err;
+        }
+    });
+    return resolve(JSON.parse(body.available));
+};
 
-ovh.request('GET', '/xdsl', function (err, ip) {
-    console.log(err || ip)
-  });
+(async()=> {
+    try {
+        let getip = await ovh.request('GET', '/ip/{ip}',{ip: 'floatingip'}, function (err, ip) {
+            console.log(err || ip);
+            return resolve(JSON.parse(body.ip));
+          });
+          do {
+              getip =await ovh.request('POST', '/ip/{ip}/move',{ip: 'floatingip'},{
+                  nexthop: null,
+                  to: server()
+              }, function(err){
+                  return err;
+              });
+            } while(!getip);
 
- 
+    } catch(err){
+        return reject(err);
+    }
+})
+
+
