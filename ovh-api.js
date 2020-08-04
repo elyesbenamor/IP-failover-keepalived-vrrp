e27fa3e873e94f1534438f43d0bf8637efb95180
Elyes Ben Amor <amor@vorwaerts.com>
2020-08-05 00:03:22 +0200

    adding js script

---
+++ ovh-api.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const ovh = require('ovh')({
+    endpoint: process.env.OVH_API_ENDPOINT,
+    appKey: process.env.OVH_API_AK,
+    appSecret: process.env.OVH_API_AS,
+    consumerKey: process.env.OVH_API_CK,
+});
+
+let datacenternames = [];
+
+const main = async () => {
+    try {
+        
+        ovh.request('GET', '/dedicated/server/availabilities', {
+            country: 'EU',
+            hardware: ''
+        }, (err, valids) => {
+            if (err) throw err;
+            
+            valids.forEach(el=>{
+                el.datacenters.filter(el => el.availability !== "unavailable").forEach(el=>datacenternames.push(el))
+            })
+
+            console.log(datacenternames)
+            var item = datacenternames[(Math.random() * datacenternames.length) | 0];
+         
+            ovh.request('GET', '/ip'/`${item}`, { ip: 'floatingip' }, function (err, ip) {
+                console.log(JSON.stringify(err))
+                if (err) return;
+                do {     
+                    ovh.request('POST', '/ip/{ip}/move', { ip: 'floatingip' }, {
+                        nexthop: null,
+                        to: item 
+                    }, function (err) {
+                        return err;
+                    });
+
+                } while (!ip);
+
+            });
+        });
+
+    } catch (err) {
+        return reject(err);
+    }
+};
+main();
