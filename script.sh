2e92e0f7c5c5e5ec6d13691636e7c0ffbfd4b126
Elyes Ben Amor <amor@vorwaerts.com>
2020-08-18 12:38:22 +0200

    update keepalived config and bash

--- script.sh
+++ script.sh
@@ -30,34 +30,4 @@ echo "[TASK 4] restart service"
 sudo systemctl restart systemd-networkd
 
 echo "[TASK 5] add configuration"
-sudo cat >>/etc/keepalived/keepalived.conf<<EOF
-global_defs {
-    enable_script_security
-    script_user node
-}
-vrrp_script chk_haproxy {
-    script "/usr/bin/pkill -0 haproxy"
-    interval 2
-    weight 2
-}
-
-vrrp_instance VI_1 {
-    interface x
-    state MASTER
-    priority 101
-    virtual_router_id 42
-    unicast_src_ip {{ masterserver_ip_address }}
-    unicast_peer {
-        {{ slave_ip_address }}
-    }
-    authentication {
-        auth_type PASS
-        auth_pass *****
-    }
-    track_script {
-        chk_haproxy
-       
-    }
-    notify_master /etc/keepalived/ovh-api.js
-}
-EOF
+sudo mv ./keepalived.conf /etc/keepalived/keepalived.conf
